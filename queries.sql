9c01fd29b8d68dafeab61cae14d1c997f02fb552
mariemcharef <167601146+mariemcharef@users.noreply.github.com>
2025-03-08 07:09:05 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -19,5 +19,19 @@ WHERE "competition" LIKE 'Hackathon challenge';
 
 --Showing each participant name with the code submitted for each problem with its judgement
 SELECT * from "participants_submissions";
---showing ho
+
+--showing the number of accepted submissions for each problem
 SELECT * from "problems_view";
+
+UPDATE competitions
+SET starting_time = '2025-04-10 14:00:00'
+WHERE name = 'Spring Challenge';
+
+UPDATE participants_competitions
+SET score = score + 10
+WHERE participant_id = (SELECT id FROM participants WHERE name = 'TeamABC')
+AND competition_id = (SELECT id FROM competitions WHERE name = 'Winter Code Fest');
+
+DELETE FROM test_cases WHERE problem_id = (SELECT id FROM problems WHERE label='B');
+
+
